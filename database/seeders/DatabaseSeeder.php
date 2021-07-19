71389fcbbcbcf9f6a893823bbeff371dbc1eb2e7
arief@bioelite.ro <arief@bioelite.ro>
2021-07-19 12:18:24 +0300

    feat: add default router

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Hash;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +16,13 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        $user = User::where("email", "test@test.com")->first();
+        if (empty($user)) {
+            User::create([
+                "name" => "Test",
+                "email" => "test@test.com",
+                "password" => Hash::make("123123")
+            ]);
+        }
     }
 }
